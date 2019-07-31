920a21d323e82fe97505e3797812523f073e7aa0
Dorelly Crisanto <dorelly.crisanto@gmail.com>
2019-07-31 17:52:36 -0500

    Funciones que retornan promesas

--- src/app/pages/promises/promises.component.ts
+++ src/app/pages/promises/promises.component.ts
@@ -8,39 +8,41 @@ import { reject } from 'q';
 })
 export class PromisesComponent implements OnInit {
 
-  constructor() { 
-    // Toda promesa recibe el resolve (ok) y el reject (error)
-    let promise = new Promise( (resolve, reject) => {
-      let counter: number = 0;
-      // Una función de js que va  hacer ciclo de tiempo y que por cada x cantidad de tiempo disparará
-      // una función
-      let interval = setInterval( () => {
-          
-          counter += 1;
-          console.log( counter );
-          if ( counter === 3 ) {
-            // Manejando el resolve
-               resolve('OK!');
-            // Manejando el reject
-            // reject('Manejando el reject en la promesa');
-            // Para que no se siga ejecutando la función
-            clearInterval( interval );
-          }
-      }, 1000);
-    });
+  constructor() {
     // 1° forma: Función para escuchar el resolve de la promesa
     /*promise.then(
       () => console.log('Termino!'),
       () => console.log('Error'),
     );*/
     // 2° forma: Para manejar el error
-    promise.then(
+    this.counter().then(
       mensaje => console.log('Termino!', mensaje)
     )
     .catch( error => console.error('Error en la promesa', error));
   }
 
   ngOnInit() {
+  }
+
+  counter(): Promise<boolean> {
+      // Toda promesa recibe el resolve (ok) y el reject (error)
+    return new Promise( (resolve, reject) => {
+      let counter: number = 0;
+      // Una función de js que va  hacer ciclo de tiempo y que por cada x cantidad de tiempo disparará
+      // una función
+      const interval = setInterval( () => {
+          counter += 1;
+          console.log( counter );
+          if ( counter === 3 ) {
+            // Manejando el resolve
+               resolve(true);
+            // Manejando el reject
+            // reject('Manejando el reject en la promesa');
+            // Para que no se siga ejecutando la función
+               clearInterval( interval );
+          }
+      }, 1000);
+    });
   }
 
 }
