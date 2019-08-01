69fc1174af4b4e51e8025eb182ca2fb54142be61
Dorelly Crisanto <dorelly.crisanto@gmail.com>
2019-08-01 14:29:32 -0500

    Operador map

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { retry } from 'rxjs/operators'
+import { Observable, Subscriber } from 'rxjs';
+import { retry, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-rxjs',
@@ -13,9 +13,10 @@ export class RxjsComponent implements OnInit {
     // Para escuchar al observable me debo subscribir
     // Para volver a intertarlo usamos retry, se vuelve a subscribir, de parámetro se puede enviar
     // la cantidad de reintentos que se requiere.
-    this.returnsObservable().pipe(
-      retry(2)
-    )
+    this.returnsObservable()
+    // .pipe(
+      // retry(2)
+    // )
     // El obs tiene 3 callback
     .subscribe(
       // 1° Callback: Cuando se llama un next, se recibe algo, lo que esté emitiendo el observador
@@ -29,30 +30,36 @@ export class RxjsComponent implements OnInit {
 
   ngOnInit() {
   }
-  
+
   // Se indica a la función que tipo devolverá
   returnsObservable(): Observable<number> {
 
      // Objeto observer de tipo subscribe pero no es necesario especificarlo
-     return new Observable( observer => {
+     return new Observable( (observer: Subscriber<any>) => {
       let counter = 0;
       let interval = setInterval( () => {
-        counter += 1;
+        counter ++;
+        const output = {
+            value: counter
+        };
         // El observable notificará mediante el next cada que llegue información.
-        observer.next( counter );
+        observer.next( output );
         if ( counter === 3 ) {
           clearInterval( interval );
           // Indicar que terminó
           observer.complete();
         }
-
-        if ( counter === 2 ) {
+        // Manejando el error
+        // if ( counter === 2 ) {
           // clearInterval( interval );
-            observer.error('Aún no llega al 3 ')
-        }
+           // observer.error('Aún no llega al 3 ')
+        // }
       }, 1000);
-    });
-    // return obs;
+
+    }).pipe(
+      // Recibo la respuesta en bruto y la transforma
+      map( resp => resp.value )
+    );
 
   }
 
