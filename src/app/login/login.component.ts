103c0f3ac6dc6c435c742dd2a67ca4376738997d
Dorelly Crisanto <dorelly.crisanto@gmail.com>
2020-02-11 12:14:15 -0500

    Obteniendo token de una autenticación con Google

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,9 +3,12 @@ import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
 import { UsuarioService } from '../services/usuario/usuario.service';
 import { User } from '../models/user.model';
+import { element } from 'protractor';
 
 // Función para que al recargar la página el loading no se quede cargando
 declare function init_plugins();
+// Indicando que existe una librería gapi
+declare const gapi: any;
 
 @Component({
   selector: 'app-login',
@@ -16,6 +19,8 @@ export class LoginComponent implements OnInit {
   // Asignamos el valor por defecto false a la variable remember
   email: string;
   remember: boolean = false;
+  // propiedad auth2, obteniendo la información que me regrese google
+  auth2: any;
 
   constructor(
               public router: Router,
@@ -24,11 +29,38 @@ export class LoginComponent implements OnInit {
 
   ngOnInit() {
      init_plugins();
+     this.googleInit();
      this.email = localStorage.getItem('email') || '';
      if (this.email.length > 1) {
       this.remember = true;
      }
-  }
+
+    }
+      // Función para la inicialización del plugin de google
+     googleInit() {
+      gapi.load('auth2', () => {
+        this.auth2 = gapi.auth2.init({
+          client_id: '1032595731724-4i8etueq6ekk49ja66b3gvi829crfut9.apps.googleusercontent.com',
+          cookiepolicy: 'single_host_origin',
+          scope: 'profile email' // información de la cuenta de google de la persona que se está logeando
+        });
+        // Obteniendo el elemento html  por el id
+        this.attachSignin(document.getElementById('btnGoogle'));
+      });
+     }
+
+    // La función recibirá un elemento html. Del callback recibo de la función el googleUser
+     attachSignin(element) {
+      this.auth2.attachClickHandler (element, {}, (googleUser) => {
+        const profile = googleUser.getBasicProfile();
+        //const token = googleUser.Zi.id_token;
+        const token = googleUser.getAuthResponse().id_token;
+        console.log(profile);
+        console.log(googleUser);
+        console.log(token);
+      });
+     }
+  
 
   login( form: NgForm ) {
     if ( form.invalid ) {
